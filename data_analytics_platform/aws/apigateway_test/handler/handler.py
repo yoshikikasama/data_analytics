ca043fa2e6df2a9c49bc2ce74407461e83ede8f3
yoshikikasama <kasamayoshiki@gmail.com>
2022-10-23 13:17:24 +0900

    20221023

--- data_analytics_platform/aws/apigateway_test/handler/handler.py
+++ data_analytics_platform/aws/apigateway_test/handler/handler.py
@@ -1,21 +1,38 @@
 import json
 
 
-def hello(event, context):
+class LambdaException(Exception):
+    def __init__(self, status_code: int, error_msg: str):
+        self.status_code = status_code
+        self.error_msg = error_msg
+
+    def __str__(self):
+        obj = {
+           'Status': self.status_code,
+            'ErrorReason': self.error_msg
+        }
+        return json.dumps(obj)
+
+
+class BadRequestException(LambdaException):
+    def __init__(self, error_msg: str):
+        super().__init__(404, error_msg)
+
+
+class InternalServerErrorException(LambdaException):
+    def __init__(self, error_msg: str):
+        super().__init__(500, error_msg)
+
+
+def lambda_handler(event, context):
+    # TODO implement
     try:
-
-        body = {
-            "message": "Go Serverless v1.0! Your function executed successfully!",
-            "input": event
+        # ERROR!
+        hoge
+        print('test')
+        return {
+            'Status' : 200, 
+            'body'       : json.dumps({'Status' : '200', 'ErrorReason' : 'None'})
         }
-
-        response = {
-            "statusCode": 200,
-            "body": json.dumps(body)
-        }
-        return response
     except Exception as e:
-        raise {
-            "statusCode": 500,
-            "body": e
-        }
+        raise InternalServerErrorException(str(e))
