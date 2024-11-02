576131011d521574b95f4990162f49aa42f040ba
yoshikikasama <kasamayoshiki@gmail.com>
2024-11-02 22:16:42 +0900

    整理

--- data_analytics_platform/aws/redshift/sql/add_partition.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE spectrum.category ADD PARTITION (year=2022,month=10,day=4) 
-LOCATION 's3://kasama-dev/category/year=2022/month=10/day=04/';
--- data_analytics_platform/aws/redshift/sql/create_spectrum_schema.sql
+++
@@ -1,5 +0,0 @@
-create external schema spectrum
-from data catalog 
-database 'dev' 
-iam_role 'arn:aws:iam::068788852374:role/service-role/AmazonRedshift-CommandsAccessRole-20221002T182645'
-create external database if not exists;
