131fb3a6b0deaf81508158b3c95670772cba5b3a
yoshikikasama <kasamayoshiki@gmail.com>
2024-11-12 22:36:11 +0000

    20241113

---
+++ SQL_recipes_for_big_data_analysis/Chapter3/query/3-4-3-クレジットカード登録と購入履歴の有無をフラグで表現.sql
@@ -0,0 +1,15 @@
+-- コード3.4.3.1：クレジットカード登録と購入履歴の有無を0,1のフラグで表現するクエリ
+SELECT
+    m.user_id
+    ,m.card_number
+    ,COUNT(p.user_id) AS purchase_count
+    -- クレジットカード番号を登録している場合は1、登録していない場合は0で表現する
+    ,CASE WHEN m.card_number IS NOT NULL THEN 1 ELSE 0 END AS has_card
+    -- 購入履歴がある場合は1、なzxい場合は8で表現する
+    ,SIGN(COUNT(p.user_id)) AS has_purchased
+FROM
+    mst_users_with_card_number AS m
+LEFT JOIN
+    purchase_log AS p
+    ON m.user_id = p.user_id
+GROUP BY m.user_id,m.card_number
---
+++ SQL_recipes_for_big_data_analysis/Chapter3/query/3-4-4-CTE.sql
@@ -0,0 +1,43 @@
+-- コード3.4.4.1：カテゴリーごとの順位を付与したテーブルに名前を付けるクエリ
+WITH
+product_sale_ranking AS (
+    SELECT
+        category_name
+        ,product_id
+        ,sales
+        ,ROW_NUMBER() OVER (PARTITION BY category_name ORDER BY sales DESC) AS rank
+    FROM
+        product_sales
+)
+
+,mst_rank AS (
+    SELECT DISTINCT rank
+    FROM product_sale_ranking
+)
+
+SELECT
+    m.rank
+    ,r1.product_id AS dvd
+    ,r1.sales AS dvd_sales
+    ,r2.product_id AS cd
+    ,r2.sales AS cd_sales
+    ,r3.product_id AS book
+    ,r3.sales AS book_sales
+FROM
+    mst_rank AS m
+LEFT JOIN
+    product_sale_ranking AS r1
+    ON
+        m.rank = r1.rank
+        AND r1.category_name = 'dvd'
+LEFT JOIN
+    product_sale_ranking AS r2
+    ON
+        m.rank = r2.rank
+        AND r2.category_name = 'cd'
+LEFT JOIN
+    product_sale_ranking AS r3
+    ON
+        m.rank = r3.rank
+        AND r3.category_name = 'book'
+ORDER BY m.rank
